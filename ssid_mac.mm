8e66cf787250a08d2d66c0ae4b7e8dae44797f38
xqq <xqq@xqq.im>
2019-03-01 21:13:44 +0900

    ssid_mac.mm: Fix null pointer crash

--- ssid_mac.mm
+++ ssid_mac.mm
@@ -5,11 +5,15 @@ std::string is_connected_to_ssid(std::initializer_list<std::string> prefer_list)
     std::string not_connected = "false";
 
     CWInterface* wifi = [[CWWiFiClient sharedWiFiClient] interface];
-    std::string ssid = [wifi.ssid UTF8String];
+    const char* ssid = [wifi.ssid UTF8String];
+
+    if (ssid == nullptr) {
+        return not_connected;
+    }
 
     for (auto& prefer : prefer_list) {
         if (ssid == prefer) {
-            printf("Interface [%s] has connected to SSID [%s]\n", [wifi.interfaceName UTF8String], ssid.c_str());
+            printf("Interface [%s] has connected to SSID [%s]\n", [wifi.interfaceName UTF8String], ssid);
             return ssid;
         }
     }
